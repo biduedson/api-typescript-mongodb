219d4e0d2c5350437f93474c4624a687913fe4c3
biduedson <112134265+biduedson@users.noreply.github.com>
2023-07-28 00:32:47 -0300

    feat: ensure GetUsersMongoRepository get users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,22 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Edson",
-        lastName: "Gomes Arruda",
-        email: "bidu@bidu.com",
-        password: "123"
-      }
-    ];
+    //aqui esta const ira pegar todos os usuarios(objetos) que estão dentro da collection('users')
+    // e transformalos em um array
+    const users = await MongoClient.db
+      //aqui esta parte <Omit<User, 'id'>> estou omitindo o campo id no retorno , pois o mongoDb
+      //usa o campo id escrito > _id .
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    // aqui estopu retornando o objeto users convertendo o campo _id em string base  Hexadecimal
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString()
+    }));
   }
 }
